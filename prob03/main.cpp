5ad448f0fd35f2bc3c17fb4a5894f2a6f9e32d28
json05 <json05@cs104u-1g.acad.fullerton.edu>
2019-09-14 14:04:00 -0700

    created sales prediction calculator

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,22 @@
 // Sales prediction
+
+#include <iostream>
+
+int main()
+{
+
+  double lastsales;
+
+  // Asks for total of last years sales
+  std::cout << "Last year's sales were $";
+  std::cin >> lastsales;
+
+  // Calculates Sales Prediction
+  double increase = lastsales * 0.18;
+  long predict = lastsales + increase;
+
+  // Displays sales prediction
+  std::cout << "This year's sales prediction: $" << predict << '\n';
+
+  return 0;
+}
